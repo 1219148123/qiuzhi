87fc603ecd54717ade2fd624a93afe1f10bd7d3f
gumingxin <2764014401@qq.com>
2020-08-30 20:22:09 +0800

    二手市场类型选择

--- pages/usedMarket/usedMarket.js
+++ pages/usedMarket/usedMarket.js
@@ -13,16 +13,89 @@ Page({
     tarbarList:['二手车','二手房'],
     // imgList:[],
     carMarketList:[],
-    houseMarketList:[]
+    houseMarketList:[],
+    tabTxt:['类型'],//tab文案
+    tab:[true],
+    typePicker:[],
+  },
+  
+  filterTab:function(e){
+    let data=[true],
+    index=e.currentTarget.dataset.index;
+    data[index]=!this.data.tab[index];
+    this.setData({
+        tab:data
+    })
+},
+//筛选项点击操作
+filter(e){
+  var self=this,
+  id=e.currentTarget.dataset.id,
+  txt=e.currentTarget.dataset.txt,
+  tabTxt=this.data.tabTxt;
+  console.log(e)
+  this.setData({
+    tab:[true]
+  })
+  switch(e.currentTarget.dataset.index){
+      case '0':
+          tabTxt[0]=txt;
+          self.setData({
+              page:1,
+              data:[],
+              tab:[true],
+              tabTxt:tabTxt,
+              house_type:id
+          });
+      break;
+  }
+  //数据筛选
+  this.getData(e);
+},
+
+getData(e){
+  console.log(e)
+  if(this.data.TabCur==1){
+    let data = {houseType:e.currentTarget.dataset.id}
+    data = JSON.stringify(data)
+    req.requestAjax('house/list','GET',data,'正在加载',(res)=>{
+      console.log(res)//请求成功回调
+      res.forEach(hInfo => {
+        hInfo.hImg = hInfo.hImg.split(',')
+      })
+      res = res.reverse()
+      this.setData({
+        houseMarketList:res
+      })
+		},function(res){
+			console.log(res)//请求失败回调
+    })
+  }else{
+    let data = {carType:e.currentTarget.dataset.id}
+    data = JSON.stringify(data)
+    req.requestAjax('secondeHandCar/list','GET',data,'正在加载',(res)=>{
+      console.log(res)//请求成功回调
+      res.forEach(carInfo => {
+        carInfo.carImg = carInfo.carImg.split(',')
+      })
+      res = res.reverse()
+      this.setData({
+        carMarketList:res
+      })
+		},function(res){
+			console.log(res)//请求失败回调
+    })
+  }
+  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  // onLoad: function (options) {
 
-  },
-  onShow(){
+  // },
+  onLoad(){
     req.requestAjax('secondeHandCar/list','GET','{}','正在加载',(res)=>{
       console.log(res)//请求成功回调
       res=res.reverse()
@@ -39,6 +112,16 @@ Page({
       console.log(this.data.carMarketList)
 		},function(res){
 			console.log(res)//请求失败回调
+    })
+
+    // cartype
+    req.requestAjax('type/carType','Get','{}','正在加载',(res)=>{
+      console.log(res)//请求成功回调
+      this.setData({
+        typePicker:res
+      })
+    },function(res){
+      console.log(res)//请求失败回调
     })
     
   },
@@ -63,6 +146,16 @@ Page({
       },function(res){
         console.log(res)//请求失败回调
       })
+
+      // housetype
+      req.requestAjax('type/houseType','Get','{}','正在加载',(res)=>{
+        console.log(res)//请求成功回调
+        this.setData({
+          typePicker:res
+        })
+      },function(res){
+        console.log(res)//请求失败回调
+      })
     }else{
 
       req.requestAjax('secondeHandCar/list','GET','{}','正在加载',(res)=>{
@@ -77,6 +170,16 @@ Page({
       },function(res){
         console.log(res)//请求失败回调
       })
+
+      // cartype
+    req.requestAjax('type/carType','Get','{}','正在加载',(res)=>{
+      console.log(res)//请求成功回调
+      this.setData({
+        typePicker:res
+      })
+    },function(res){
+      console.log(res)//请求失败回调
+    })
     }
   },
   goUsedDetail(e){
